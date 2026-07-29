cd64ce318bac39b775de4a8ead8b78cd0f07a011
Chris <chris.blair041785@gmail.com>
2026-07-29 15:27:03 -0400

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
+import React, {useState} from 'react'; 
 import './App.css';
+import Button from './Button'
+import DogTagTemplate from './assets/Dog Tag Template.png';
 
 function App() {
+const handleLines = () => {};
+const handleFont = () => {}; 
+const handleStyle = () => {};
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Dog Tag Laser Engraving Order Form </h1>
+      <Button label="Lines" onClick={handleLines} />
+      <Button label="Font" onClick={handleFont} />
+      <Button label="Style" onClick={handleStyle} /><br/>
+      <textarea placeholder="Enter your text here"></textarea><br/>
+      <img src={DogTagTemplate}
+        alt="Dog Tag Template" />
+
     </div>
   );
 }
